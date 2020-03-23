918fce339b3b5b8cd0c8b570d9ecda17a2679bdd
AnnaTruzzi <anna.truzzi91@gmail.com>
2020-03-23 10:59:09 +0000

    initial commit

---
+++ main.sh
@@ -0,0 +1,31 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+#!/bin/bash
+
+
+#SBATCH --cpus-per-task=32
+#SBATCH --gpus-per-task=1
+#SBATCH -J train_dc_noSobel
+#SBATCH --output=/dc_noSobel/logs/slurm-%j.out
+#SBATCH --error=/dc_noSobel/logs/slurm-%j.err
+
+
+DIR="/home/CUSACKLAB/annatruzzi/imagenet_sample"
+ARCH="alexnet"
+LR=0.05
+WD=-5
+K=10000
+K=10
+WORKERS=4
+EXP="/home/CUSACKLAB/annatruzzi/deepcluster_noSobel/checkpoints"
+#PYTHON="/home/CUSACKLAB/annatruzzi/anaconda3/envs/pytorch_p27/bin/python"
+PYTHON = "pyhton"
+
+mkdir -p ${EXP}
+
+CUDA_VISIBLE_DEVICES=0 ${PYTHON} main.py ${DIR} --exp ${EXP} --arch ${ARCH} \
+  --lr ${LR} --wd ${WD} --k ${K} --sobel --verbose --workers ${WORKERS}
