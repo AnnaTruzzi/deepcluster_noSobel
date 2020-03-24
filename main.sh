c64ddaab35f62bc426aa2bfaa1f2a5a99291e850
Anna Truzzi <anna.truzzi91@gmail.com>
2020-03-24 12:09:27 +0100

    update varaibles to run on the server

--- main.sh
+++ main.sh
@@ -7,14 +7,13 @@
 #!/bin/bash
 
 
-#SBATCH --cpus-per-task=32
 #SBATCH --gpus-per-task=2
 #SBATCH -J train_dc_noSobel
 #SBATCH --output=/dc_noSobel/logs/slurm-%j.out
 #SBATCH --error=/dc_noSobel/logs/slurm-%j.err
 
 
-DIR="/home/CUSACKLAB/annatruzzi/imagenet_sample"
+DIR="/data/ILSVRC2012/train"
 ARCH="alexnet"
 LR=0.05
 WD=-5
@@ -22,12 +21,9 @@ K=10000
 K=10
 WORKERS=2
 EXP="/home/CUSACKLAB/annatruzzi/deepcluster_noSobel/checkpoints"
-#PYTHON="/home/CUSACKLAB/annatruzzi/anaconda3/envs/pytorch_p27/bin/python"
-PYTHON = "pyhton"
+PYTHON="/opt/anaconda3/envs/dc_p27/bin/python"
 
 mkdir -p ${EXP}
-
-source /home/annatruzzi/conda_env_config/create_pytorch_p27.sh
 
 CUDA_VISIBLE_DEVICES=2 ${PYTHON} main.py ${DIR} --exp ${EXP} --arch ${ARCH} \
   --lr ${LR} --wd ${WD} --k ${K} --sobel --verbose --workers ${WORKERS}
